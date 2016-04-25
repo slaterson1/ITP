9726741e94e15e14c73f2ff99f7dd42d56e261aa
DaveKeith <d.keith78@yahoo.com>
2016-04-25 13:18:00 -0400

    updated application_controller, meant to do this earlier

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,24 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+    protect_from_forgery with: :null_session
+
+  def current_user
+  	token = request.headers["X-Auth-Token"]
+  	if token
+  		User.find_by(auth_token: token)
+  	end
+  end
+
+  def authenticate!
+  	unless current_user
+  		render json: { errors: "YOU MUST LOG IN FIRST!" },
+  			status: :unauthorized
+  	end
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do |error|
+       render json: { error: "No such object: #{error.message} " },
+      status: :not_found
+    end
 end

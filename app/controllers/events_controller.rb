6734e62807c3d3ec474cc2d531a49f14c77aa76b
DaveKeith <d.keith78@yahoo.com>
2016-04-25 14:44:02 -0400

    created pitstop and events model, updated events controller to include price filter and create (both incomplete)

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,12 @@
+class EventsController < ApplicationController
+  before_action :authenticate!
+
+  def create
+    @itinerary = Itinerary.find["id"]
+    @event = @itinerary.events.create()
+  end
+
+  def price_filter
+    @event = Event.where("price < :price", price: params[:price])
+  end
+end
---
+++ test/models/pitstop_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PitstopTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

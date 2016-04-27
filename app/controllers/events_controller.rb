d152d8ed9f7bcb2df820e69c06a6cd67d153a5cd
DaveKeith <d.keith78@yahoo.com>
2016-04-27 15:50:11 -0400

    invalid dates now give errors correctly

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,40 +12,32 @@ class EventsController < ApplicationController
 
   def first_event
     zip = params[:zip]
-    local_datetime = "2000-01-01"
-    d = Date.parse(due_date)
-    while !real_date?(d.year, d.month, d.day)
-      render json: { errors: "Enter the date in which you would like to accomplish this task YYYY-MM-DD format:" },
-      local_datetime = gets.chomp
-      begin
-        d = Date.parse(local_datetime)
-      rescue ArgumentError
-      end
-    end
     local_datetime = params[:local_datetime]
-  	s = Seatgeek.new(zip, local_datetime)
-    seatgeek = s.get_first_game
-    render json: seatgeek,
-    status: :ok
+    d = Date.parse(local_datetime)
+    if !real_date?(d.year, d.month, d.day)
+      render json: { errors: "Date must be relevant and in YYYY-MM-DD format" },
+                  status: :unauthorized
+    else
+    	s = Seatgeek.new(zip, local_datetime)
+      seatgeek = s.get_first_game
+      render json: seatgeek,
+      status: :ok
+    end
   end
 
   def next_event
     zip = params[:zip]
-    local_datetime = "2000-01-01"
-    d = Date.parse(due_date)
-    while !real_date?(d.year, d.month, d.day)
-      render json: { errors: "Enter the date in which you would like to accomplish this task YYYY-MM-DD format:" },
-      local_datetime = gets.chomp
-      begin
-        d = Date.parse(local_datetime)
-      rescue ArgumentError
-      end
-    end
     local_datetime = params[:local_datetime]
-  	s = Seatgeek.new(zip, local_datetime)
-  	seatgeek = s.get_games
-    render json: seatgeek,
-    status: :ok
+    d = Date.parse(local_datetime)
+    if !real_date?(d.year, d.month, d.day)
+      render json: { errors: "Date must be relevant and in YYYY-MM-DD format" },
+                  status: :unauthorized
+    else
+    	s = Seatgeek.new(zip, local_datetime)
+    	seatgeek = s.get_games
+      render json: seatgeek,
+      status: :ok
+    end
   end
 
 end

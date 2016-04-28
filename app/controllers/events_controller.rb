43ea4d1c17e197c53304b14c73ebf33e67111cc3
DaveKeith <d.keith78@yahoo.com>
2016-04-28 14:22:10 -0400

    created methods create_first_event and create_next_event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,41 @@
 class EventsController < ApplicationController
   before_action :authenticate!
 
-  def create
-    itinerary = Itinerary.find["id"]
-    @event = itinerary.events.create()
+  def create_first_event
+    zip = params[:zip]
+    local_datetime = params[:local_datetime]
+    d = Date.parse(local_datetime)
+    if !real_date?(d.year, d.month, d.day)
+      render json: { errors: "Date must be relevant and in YYYY-MM-DD format" },
+                  status: :unauthorized
+    else
+      itinerary = current_user.itineraries.create(start_date: local_datetime,
+  																	             zip: zip,
+  																	             travel_days: 1)
+      @pitstop = itinerary.pitstops.create(zip: zip,
+                                          stop_number: 1)
+      @event = @pitstop.events.create(zip: zip,
+                                      local_datetime: local_datetime)
+      render "create.json.jbuilder", status: :created
+    end
+  end
+
+  def create_next_event
+    zip = params[:zip]
+    local_datetime = params[:local_datetime]
+    d = Date.parse(local_datetime)
+    if !real_date?(d.year, d.month, d.day)
+      render json: { errors: "Date must be relevant and in YYYY-MM-DD format" },
+                  status: :unauthorized
+    else
+      itinerary = current_user.itineraries.last
+      @pitstop = itinerary.pitstops.new(zip: zip,
+                                        stop_number: (itinerary.stop_number + 1))
+      itinerary.update(travel_days: (itinerary.travel_days + 1))
+      @event = @pitstop.events.create(zip: zip,
+                                      local_datetime: local_datetime)
+      render "create.json.jbuilder", status: :created
+    end
   end
 
   def price_filter

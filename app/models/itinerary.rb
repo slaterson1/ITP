82ffe3f9d2c3b6366faef1864afac232bf7a8c8d
DaveKeith <d.keith78@yahoo.com>
2016-04-28 09:47:09 -0400

    added date regex to itinerary and pitstop

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -5,4 +5,8 @@ class Itinerary < ActiveRecord::Base
 	validates :start_city, presence: true
 	validates :user_id, presence: true
 	validates :start_date, presence: true
+	format: {
+    with: \d\d\d\d-\d\d-\d\d,
+    message: "USE VALID DATE FORMAT 'YYYY-MM-DD'."
+  }
 end
--- app/models/pitstop.rb
+++ app/models/pitstop.rb
@@ -4,6 +4,10 @@ class Pitstop < ActiveRecord::Base
 
   validates :city, presence: true
   validates :date_visited, presence: true
+  format: {
+    with: \d\d\d\d-\d\d-\d\d,
+    message: "USE VALID DATE FORMAT 'YYYY-MM-DD'."
+  }
   validates :itinerary_id, presence: true
   validates :stop_number, presence: true
 end

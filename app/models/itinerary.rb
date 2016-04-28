5ca17ecb5043021e5cc644c33ea3ce95b2da1851
DaveKeith <d.keith78@yahoo.com>
2016-04-28 10:22:28 -0400

    I think these new regexes should work

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -2,11 +2,11 @@ class Itinerary < ActiveRecord::Base
 	has_many :pitstops
 	belongs_to :user
 
-	# validates :start_city, presence: true
-	# validates :user_id, presence: true
-	# validates :start_date, presence: true,
-	# format: {
-  #   with: \d\d\d\d-\d\d-\d\d,
-  #   message: "USE VALID DATE FORMAT 'YYYY-MM-DD'."
-  # }
+	validates :start_city, presence: true
+	validates :user_id, presence: true
+	validates :start_date, presence: true,
+	format: {
+    with: /\d{4}-\d{2}-\d{2}/,
+    message: "USE VALID DATE FORMAT 'YYYY-MM-DD'."
+  }
 end

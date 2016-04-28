19216e4f9245050a6b48a9120fd3f3f6f44657cb
DaveKeith <d.keith78@yahoo.com>
2016-04-28 10:02:48 -0400

    missed a comma

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,10 @@
 class Event < ActiveRecord::Base
   belongs_to :pitstop
 
-  
+
   validates :zip, presence: true
   format: {
-    with: \d\d\d\d\d
+    with: \d\d\d\d\d,
     message: "USE VALID 5-DIGIT ZIP CODE."
   }
   validates :local_datetime, presence: true

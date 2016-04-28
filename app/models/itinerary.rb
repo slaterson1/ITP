074145a56ed19df270cde8d9cf63c68431ca0c01
DaveKeith <d.keith78@yahoo.com>
2016-04-28 12:21:41 -0400

    travel_days validation now assures that there is at least 1 day

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -9,5 +9,5 @@ class Itinerary < ActiveRecord::Base
     with: /\d{4}-\d{2}-\d{2}/,
     message: "USE VALID DATE FORMAT 'YYYY-MM-DD'."
   }
-	validates :travel_days, presence: true
+	validates :travel_days, presence: true, in: 1..10000
 end

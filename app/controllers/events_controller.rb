fc22276168f8553a9836aa1be769e8e6272816c1
DaveKeith <d.keith78@yahoo.com>
2016-04-28 16:50:13 -0400

    create_next_event now updates dates correctly

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -26,22 +26,33 @@ class EventsController < ApplicationController
 
   def create_next_event
     zip = params[:zip]
-    local_datetime = params[:local_datetime]
-    d = Date.parse(local_datetime)
-    if !real_date?(d.year, d.month, d.day)
-      render json: { errors: "Date must be relevant and in YYYY-MM-DD format" },
-                  status: :unauthorized
+    @itinerary = current_user.itineraries.last
+    previous_date = @itinerary.pitstops.last.date_visited
+    previous_date = previous_date.to_date
+    previous_date = previous_date.to_s
+    d = Date.parse(previous_date)
+    new_day = d.day + 1
+    new_date = ""
+    if !real_date?(d.year, d.month, new_day)
+      new_day = 1
+      new_month = d.month + 1
+      new_date = "#{d.year}-#{new_month}-#{new_day}"
+      if !real_date?(d.year, new_month, new_day)
+        new_year = d.year + 1
+        new_date = "#{new_year}-#{new_month}-#{new_day}"
+      end
     else
-      @itinerary = current_user.itineraries.last
-      @pitstop = @itinerary.pitstops.new(zip: zip,
-                                        stop_number: (itinerary.stop_number + 1))
-      @pitstop.save
-      @itinerary.update(travel_days: (itinerary.travel_days + 1))
-      @event = @pitstop.events.new(zip: zip,
-                                      local_datetime: local_datetime)
-      @event.save
-      render "create.json.jbuilder", status: :created
+      new_date = "#{d.year}-#{d.month}-#{new_day}"
     end
+    @pitstop = @itinerary.pitstops.new(zip: zip,
+                                      stop_number: (@itinerary.pitstops.last.stop_number + 1),
+                                      date_visited: new_date)
+    @pitstop.save!
+    @itinerary.update(travel_days: (@itinerary.travel_days + 1))
+    @event = @pitstop.events.new(zip: zip,
+                                local_datetime: new_date)
+    @event.save!
+    render "create.json.jbuilder", status: :created
   end
 
   def price_filter

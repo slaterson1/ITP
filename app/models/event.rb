2b0fee24a9b9d94ffae6efc94521527c91a201fd
DaveKeith <d.keith78@yahoo.com>
2016-04-28 10:12:27 -0400

    still having issues, commented out regexes so that we can proceed with testing

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,14 +2,14 @@ class Event < ActiveRecord::Base
   belongs_to :pitstop
 
 
-  validates :zip, presence: true,
-  format: {
-    with: \d\d\d\d\d,
-    message: "USE VALID 5-DIGIT ZIP CODE."
-  }
-  validates :local_datetime, presence: true,
-	format: {
-    with: \d\d\d\d-\d\d-\d\d,
-    message: "USE VALID DATE FORMAT 'YYYY-MM-DD'."
-  }
+  # validates :zip, presence: true,
+  # format: {
+  #   with: \d\d\d\d\d,
+  #   message: "USE VALID 5-DIGIT ZIP CODE."
+  # }
+  # validates :local_datetime, presence: true,
+	# format: {
+  #   with: \d\d\d\d-\d\d-\d\d,
+  #   message: "USE VALID DATE FORMAT 'YYYY-MM-DD'."
+  # }
 end
--- app/models/pitstop.rb
+++ app/models/pitstop.rb
@@ -3,11 +3,11 @@ class Pitstop < ActiveRecord::Base
   belongs_to :itinerary
 
   validates :city, presence: true
-  validates :date_visited, presence: true,
-  format: {
-    with: \d\d\d\d-\d\d-\d\d,
-    message: "USE VALID DATE FORMAT 'YYYY-MM-DD'."
-  }
+  # validates :date_visited, presence: true,
+  # format: {
+  #   with: \d\d\d\d-\d\d-\d\d,
+  #   message: "USE VALID DATE FORMAT 'YYYY-MM-DD'."
+  # }
   validates :itinerary_id, presence: true
   validates :stop_number, presence: true
 end

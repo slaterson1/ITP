eb7e54d6e15f241313014bc09254f9a87ab647a1
DaveKeith <d.keith78@yahoo.com>
2016-04-28 09:29:22 -0400

    added validations for itinerary and pitstop

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,4 +1,8 @@
 class Itinerary < ActiveRecord::Base
 	has_many :pitstops
 	belongs_to :user
+
+	validates :start_city, presence: true
+	validates :user_id, presence: true
+	validates :start_date, presence: true
 end
--- app/models/pitstop.rb
+++ app/models/pitstop.rb
@@ -1,4 +1,9 @@
 class Pitstop < ActiveRecord::Base
   has_many :events
   belongs_to :itinerary
+
+  validates :city, presence: true
+  validates :date_visited, presence: true
+  validates :itinerary_id, presence: true
+  validates :stop_number, presence: true
 end

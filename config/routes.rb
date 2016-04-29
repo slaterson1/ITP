4c70e97b2cb16d02b6c3ead03325b399c9a7c9f4
DaveKeith <d.keith78@yahoo.com>
2016-04-29 13:03:01 -0400

    added itinerary id to pitstop_show route

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ post "nextgame", to: "events#create_next_event"
 post "firstgamedata", to: "events#first_event"
 post "nextgamedata", to: "events#next_event"
 post "itinerary", to: "itinerary#show"
-post "pitstop", to: "pitstops#show"
+post "itinerary/:id/pitstop_show", to: "pitstops#show"
   # Example resource route with options:
   #   resources :products do
   #     member do

ca29bd5eae13011a12dec1ef41ee8034a290e9e1
DaveKeith <d.keith78@yahoo.com>
2016-05-03 14:36:22 -0400

    made a basic create method

--- app/controllers/pitstops_controller.rb
+++ app/controllers/pitstops_controller.rb
@@ -3,14 +3,7 @@ class PitstopsController < ApplicationController
 
   def create
     itinerary = Itinerary.find[:id]
-    if itinerary.pitstops.last
-      i = itinerary.pitstops.last
-      @pitstop = itinerary.pitstops.new(city: params['city'],
-                                        stop_number: (i.stop_number + 1))
-    else
-      @pitstop = itinerary.pitstops.new(city: params['city'],
-                                        stop_number: 1)
-    end
+    @pitstop = itinerary.pitstops.new(city: params['city'])
     render "create.json.jbuilder", status: :created
   end
 

bbc322bd1f6ac70c3bd7da6bd9ed60fd7e0cace3
DaveKeith <d.keith78@yahoo.com>
2016-05-03 14:15:44 -0400

    ran rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160428175213) do
+ActiveRecord::Schema.define(version: 20160503145716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160428175213) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "pitstop_id"
+    t.integer  "game_number"
   end
 
   create_table "itineraries", force: :cascade do |t|

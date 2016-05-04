d131d0251863b5d02d91968b7261c17dd348e685
slatersan1 <cdlee212@gmail.com>
2016-05-04 17:00:13 -0400

    edit stuff

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,8 +3,11 @@ class EventsController < ApplicationController
 
   def create
     @itinerary = current_user.itineraries.find_or_create_by(user_id: current_user.id)
+    Rails.logger.info "@interary is #{@itinerary}"
     @pitstop = @itinerary.pitstops.find_or_create_by(itinerary_id: @itinerary.id)
+    Rails.logger.info "@pitstop is #{@pitstop}"
     @event = @pitstop.events.create(local_datetime: params["local_datetime"], game_number: params["game_number"])
+    Rails.logger.info "@event is #{@event}"
     
     s = Seatgeek.new(@pitstop.date_visited)
     seatgeek = s.get_games

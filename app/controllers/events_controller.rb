8e4ec7a830abc0736059b4017dd4ecda703b68d7
slatersan1 <cdlee212@gmail.com>
2016-05-04 15:16:48 -0400

    took out binding.pry

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,6 +6,5 @@ class EventsController < ApplicationController
     @pitstop = @itinerary.pitstops.where(itinerary_id: @itinerary.id).first_or_create
     @event = @pitstop.events.create(local_datetime: params["local_datetime"], game_number: params["game_number"])
     render "create.json.jbuilder", status: :created
-  	binding.pry
   end
 end

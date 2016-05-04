2c3d76c52d9f1bb484e0de385077cbdf6f991456
slatersan1 <cdlee212@gmail.com>
2016-05-04 16:10:31 -0400

    edit events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   before_action :authenticate!
 
   def create
-    @itinerary = current_user.itineraries.where(user_id: user.id).first_or_create
+    @itinerary = current_user.itineraries.where(user_id: current_user.id).first_or_create
     @pitstop = @itinerary.pitstops.where(itinerary_id: @itinerary.id).first_or_create
     @event = @pitstop.events.create(local_datetime: params["local_datetime"], game_number: params["game_number"])
     render "create.json.jbuilder", status: :created

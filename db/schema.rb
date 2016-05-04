43fe16919f88184f66fbb76edba2484705eab559
DaveKeith <d.keith78@yahoo.com>
2016-05-04 09:26:28 -0400

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503145716) do
+ActiveRecord::Schema.define(version: 20160504130851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,12 +37,10 @@ ActiveRecord::Schema.define(version: 20160503145716) do
 
   create_table "itineraries", force: :cascade do |t|
     t.date     "start_date"
-    t.string   "start_city"
     t.integer  "travel_days"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
-    t.integer  "zip"
   end
 
   create_table "pitstops", force: :cascade do |t|
@@ -51,8 +49,6 @@ ActiveRecord::Schema.define(version: 20160503145716) do
     t.datetime "date_visited"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.integer  "stop_number"
-    t.integer  "zip"
   end
 
   create_table "users", force: :cascade do |t|

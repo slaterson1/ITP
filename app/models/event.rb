87608d4a678772bc770cf531d0dac493fdefe34a
slatersan1 <cdlee212@gmail.com>
2016-05-04 10:02:24 -0400

    edit validations to remove zip

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,6 @@
 class Event < ActiveRecord::Base
   belongs_to :pitstop
 
-
-  validates :zip, presence: true,
-  format: {
-    with: /\d{5}/,
-    message: "USE VALID 5-DIGIT ZIP CODE."
-  }
   validates :local_datetime, presence: true,
 	format: {
     with: /\d{4}-\d{2}-\d{2}/,
--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -2,12 +2,10 @@ class Itinerary < ActiveRecord::Base
 	has_many :pitstops
 	belongs_to :user
 
-	validates :zip, presence: true
 	validates :user_id, presence: true
 	validates :start_date, presence: true,
 	format: {
     with: /\d{4}-\d{2}-\d{2}/,
     message: "USE VALID DATE FORMAT 'YYYY-MM-DD'."
   }
-	validates :travel_days, presence: true
 end

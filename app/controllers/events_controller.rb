3c0f5bfe47176f6ee83c21528156656cad16a1d1
DaveKeith <d.keith78@yahoo.com>
2016-05-06 14:02:19 -0400

    create now updates pitstop and event.  Got rid of the params methods

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,16 +3,27 @@ class EventsController < ApplicationController
 
   def create
     @itinerary = current_user.itineraries.find(params[:itinerary_id])
-    @pitstop = @itinerary.pitstops.create(date_visited: date_visited)
+    @pitstop = @itinerary.pitstops.create(date_visited: params["date_visited"])
+  	s = Seatgeek.new(@pitstop.date_visited.to_date - 1.day)
     @event = @pitstop.events.create(game_number: params["game_number"])
 
-    @pitstop.update(pitstop_params(@event.game_number))
-    @event.update(event_params(@event.game_number))
-    # @event.update_from_seatgeek
+    @pitstop.update(city: "#{s.get_city(@event.game_number)}, #{s.get_state(@event.game_number)} #{s.get_zip(@event.game_number)}")
+    @event.update(team: s.get_team(@event.game_number),
+                  gps_location: s.get_gps_location(@event.game_number),
+                  city: s.get_city(@event.game_number),
+                  state: s.get_state(@event.game_number),
+                  zip: s.get_zip(@event.game_number),
+                  street_address: s.get_street_address(@event.game_number),
+                  low_price: s.get_low_price(@event.game_number),
+                  high_price: s.get_high_price(@event.game_number),
+                  average_price: s.get_average_price(@event.game_number),
+                  venue_photo: s.get_venue_photo(@event.game_number),
+                  ticket_url: s.get_ticket_url(@event.game_number),
+                  local_datetime: s.get_local_datetime(@event.game_number))
 
-	render :json => { :itinerary => @itinerary.id,
-      			      :pitstop => @pitstop.date_visited,
-      			      :event => @event }
+  	render :json => { :itinerary => @itinerary.id,
+        			      :pitstop => @pitstop.date_visited,
+        			      :event => @event }
   end
 
   def next_event
@@ -26,31 +37,6 @@ class EventsController < ApplicationController
     render :json => { :local_datetime => s.get_local_datetime(game_number).to_date,
                       :seatgeek => @seatgeek }
 
-  end
-
-  private
-  def pitstop_params(game_number)
-    @pitstop = @itinerary.pitstops.last
-  	s = Seatgeek.new(@pitstop.date_visited.to_date - 1.day)
-    params.permit(city: "#{s.get_city(game_number)}, #{s.get_state(game_number)} #{s.get_zip(game_number)}")
-  end
-
-  def event_params(game_number)
-    @pitstop = @itinerary.pitstops.last
-  	s = Seatgeek.new(@pitstop.date_visited.to_date - 1.day)
-  	params.permit(team: s.get_team(game_number),
-                  gps_location: s.get_gps_location(game_number),
-                  city: s.get_city(game_number),
-                  state: s.get_state(game_number),
-                  zip: s.get_zip(game_number),
-                  street_address: s.get_street_address(game_number),
-                  low_price: s.get_low_price(game_number),
-                  high_price: s.get_high_price(game_number),
-                  average_price: s.get_average_price(game_number),
-                  venue_photo: s.get_venue_photo(game_number),
-                  ticket_url: s.get_ticket_url(game_number),
-                  local_datetime: s.get_local_datetime(game_number)
-                  )
   end
 
 end
